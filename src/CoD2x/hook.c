192f90d0f78fc482527ac685dfa7f957644f646f
eyza <kratas.tom@seznam.cz>
2024-11-15 22:56:04 +0100

    Fixed black screen on game start caused by missing microphone / sound device

--- src/CoD2x/hook.c
+++ src/CoD2x/hook.c
@@ -51,6 +51,15 @@ BOOL Hook_Load() {
     // Patched:  e9c64b0500  call GfxLoadDll
     patch_call(0x004102b5, (int)GfxLoadDll);
 
+    // Patch black screen / long loading on game startup
+    // Caused by Windows Mixer loading
+    // For some reason function mixerGetLineInfoA() returns a lot of connections, causing it to loop for a long time
+    // This patch skips the whole function what processs the mixer
+    // Original: 7222  jb 0x4b956b
+    // Patched:  EB22  jmp 0x4b956b
+    patch_byte(0x004b9547, 0xEB);
+
+
 
     //MessageBox(NULL, "Memory patched successfully!", "Info", MB_OK | MB_ICONINFORMATION);
     return TRUE;
