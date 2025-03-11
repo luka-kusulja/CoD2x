a2ea0b3f558a672923b0bc117e34feff853bf9ef
eyza <kratas.tom@seznam.cz>
2025-03-11 20:52:28 +0100

    Template for new .cpp and .h files

---
+++ src/_template.cpp
@@ -0,0 +1,26 @@
+#include "_template.h"
+
+// Include system headers
+#include <iostream>
+#include <string>
+
+// Include internal project headers, shared first
+#include "shared.h"
+#include "_template.h"
+
+// Define external variables
+int globalVariable = 0;
+
+
+/** Called every frame on frame start. */
+void template_frame() {
+}
+
+/** Called only once on game start after common inicialization. Used to initialize variables, cvars, etc. */
+void template_init() {
+}
+
+/** Called before the entry point is called. Used to patch the memory. */
+void template_patch() {
+
+}
---
+++ src/_template.h
@@ -0,0 +1,19 @@
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+// Include system headers
+#include <iostream>
+#include <string>
+
+// Include shared headers
+#include "shared.h"
+
+// Function declarations
+void template_frame();
+void template_init();
+void template_patch();
+
+// Global variables
+extern int globalVariable;
+
+#endif // TEMPLATE_H
